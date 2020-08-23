d0bbbbd36509831a22a5bcacea4f386253e203b7
Ishaan-11 <ishaansharma1998@gmail.com>
2020-08-23 16:05:12 +0530

    Inserting data using mongoose

--- app.js
+++ app.js
@@ -1,66 +1,19 @@
-const { MongoClient } = require("mongodb");
+const mongoose = require('mongoose');
 
-// Connection URI
-const uri =
-  "mongodb://localhost:27017";
+mongoose.connect("mongodb://localhost:27017/fruitsDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
-// Create a new MongoClient
-const client = new MongoClient(uri,{useNewUrlParser: true, useUnifiedTopology: true});
+const fruitSchema = mongoose.Schema({
+  name: String,
+  rating: Number,
+  review: String
+});
 
-async function run() {
-  try {
-    // Connect the client to the server
-    await client.connect();
+const Fruit = mongoose.model("Fruit", fruitSchema);
 
-    // Establish and verify connection
-    const database = client.db("fruitsDB");
-    console.log("Connected successfully to server");
-    // await insertDocument(database);
-    await findDocument(database);
+const fruit = new Fruit({
+  name: "Apple",
+  rating: 7,
+  review: "Pretty solid as a fruit"
+});
 
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
-
- async function insertDocument(database, callback) {
-  //get the documents collection
-  const collection = database.collection("fruits");
-  //insert some documents
-  const fruitsDocuments = [
-    { 
-      name: "Apple",
-      score: 8,
-      review: "Great fruit"
-    },
-    { 
-      name: "Orange",
-      score: 6,
-      review: "Kinda sour"
-    },
-    { 
-      name: "Banana",
-      score: 9,
-      review: "Great stuff!"
-    }
-  ];
-  const result = await collection.insertMany(fruitsDocuments);
-}
-
-async function findDocument(database, callback) {
-  //get the documents collection
-  const collection = database.collection("fruits");
-  // Query for a fruits that has the title 'Apple'
-  const query = { name: "Apple" };
-
-  const options = {
-    // Include only the `name` and `score` fields in the returned document
-    projection: { _id: 0, name: 1, score: 1 },
-  };
-  const result = await collection.findOne(query, options);
-  // since this method returns the matched document, not a cursor, print it directly
-  console.log(result);
-}
-
+fruit.save();
