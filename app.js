05dc8d9df3990255a611f795efdb115baf71419c
Ishaan-11 <ishaansharma1998@gmail.com>
2020-08-23 17:05:03 +0530

    Data validation with  mongoose

--- app.js
+++ app.js
@@ -2,23 +2,31 @@ const mongoose = require('mongoose');
 
 mongoose.connect("mongodb://localhost:27017/fruitsDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
-const fruitSchema = mongoose.Schema({
-  name: String,
-  rating: Number,
+const fruitSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Why no Name?"]
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 10
+  },
   review: String
 });
 
 const Fruit = mongoose.model("Fruit", fruitSchema);
 
 const fruit = new Fruit({
-  name: "Apple",
+  // name: "Apple",
   rating: 7,
+  // rating: 34,
   review: "Pretty solid as a fruit"
 });
 
-// fruit.save();
+fruit.save();
 
-const personSchema = mongoose.Schema({
+const personSchema = new mongoose.Schema({
   name: String,
   age: Number
 });
@@ -31,32 +39,6 @@ const person = new Person({
 });
 
 // person.save();
-
-const kiwi = new Fruit({
-  name: "Kiwi",
-  rating: 10,
-  review: "The Best fruit"
-});
-
-const orange = new Fruit({
-  name: "Orange",
-  rating: 4,
-  review: "Too sour for me"
-});
-
-const banana = new Fruit({
-  name: "Banana",
-  rating: 3,
-  review: "Weird texture"
-});
-
-/* Fruit.insertMany([kiwi, orange, banana], function(err) {
-  if(err) {
-    console.log(err);
-  } else {
-    console.log('Successfully saved all the fruits')
-  }
-}); */
 
 Fruit.find(function(err, fruits) {
   if(err) {
