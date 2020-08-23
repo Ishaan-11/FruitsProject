79f5cd5dff5207152472aa07a9dc45e9d0ca38a4
Ishaan-11 <ishaansharma1998@gmail.com>
2020-08-23 16:43:02 +0530

    Reading from DB using mongoose

--- app.js
+++ app.js
@@ -16,4 +16,56 @@ const fruit = new Fruit({
   review: "Pretty solid as a fruit"
 });
 
-fruit.save();
+// fruit.save();
+
+const personSchema = mongoose.Schema({
+  name: String,
+  age: Number
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person({
+  name: "John",
+  age: 37
+});
+
+// person.save();
+
+const kiwi = new Fruit({
+  name: "Kiwi",
+  rating: 10,
+  review: "The Best fruit"
+});
+
+const orange = new Fruit({
+  name: "Orange",
+  rating: 4,
+  review: "Too sour for me"
+});
+
+const banana = new Fruit({
+  name: "Banana",
+  rating: 3,
+  review: "Weird texture"
+});
+
+/* Fruit.insertMany([kiwi, orange, banana], function(err) {
+  if(err) {
+    console.log(err);
+  } else {
+    console.log('Successfully saved all the fruits')
+  }
+}); */
+
+Fruit.find(function(err, fruits) {
+  if(err) {
+    console.log(err);
+  } else {
+    // console.log(fruits);
+    fruits.forEach(function(fruit) {
+      mongoose.connection.close();
+      console.log(fruit.name);
+    });
+  }
+});
